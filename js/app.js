63d66dd19bca5f98b4e52b7f21db9daa455c9c61
Maria-Elena Farmer <mfarmer_89@hotmail.com>
2019-07-15 16:01:25 -0400

    first method added

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ var store1 {
   location: "1st and Pike",
   minCustHr: 23,
   maxCustHr: 65,
-  avgCookieSale: 6.3
+  avgCookieSale: 6.3,
+  randNoCust: randomNoCustomersHr
 };
 
 //store 2
@@ -13,7 +14,8 @@ var store2 {
   location: "SeaTac Airport",
   minCustHr: 3,
   maxCustHr: 24,
-  avgCookieSale: 1.2
+  avgCookieSale: 1.2,
+  randNoCust: randomNoCustomersHr
 };
 
 //store 3
@@ -21,7 +23,8 @@ var store3 {
   location: "Seattle Center",
   minCustHr: 11,
   maxCustHr: 38,
-  avgCookieSale: 3.7
+  avgCookieSale: 3.7,
+  randNoCust: randomNoCustomersHr
 };
 
 //store 4
@@ -29,7 +32,8 @@ var store4 {
   location: "Capitol Hill",
   minCustHr: 20,
   maxCustHr: 38,
-  avgCookieSale: 2.3
+  avgCookieSale: 2.3,
+  randNoCust: randomNoCustomersHr
 };
 
 //store 5
@@ -37,5 +41,12 @@ var store5 {
   location: "Alki",
   minCustHr: 2,
   maxCustHr: 16,
-  avgCookieSale: 4.6
+  avgCookieSale: 4.6,
+  randNoCust: randomNoCustomersHr
 };
+
+function randomNoCustomersHr() {
+  min = Math.ceil(this.minCustHr);
+  max = Math.floor(this.maxCustHr);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
