00e41d8fbc89d99015bbf9ecba42c4cd4b1b6ca6
Maria-Elena Farmer <mfarmer_89@hotmail.com>
2019-07-15 14:38:38 -0400

    object literals added with store info

---
+++ js/app.js
@@ -0,0 +1,41 @@
+usestrict;
+
+//store 1
+var store1 {
+  location: "1st and Pike",
+  minCustHr: 23,
+  maxCustHr: 65,
+  avgCookieSale: 6.3
+};
+
+//store 2
+var store2 {
+  location: "SeaTac Airport",
+  minCustHr: 3,
+  maxCustHr: 24,
+  avgCookieSale: 1.2
+};
+
+//store 3
+var store3 {
+  location: "Seattle Center",
+  minCustHr: 11,
+  maxCustHr: 38,
+  avgCookieSale: 3.7
+};
+
+//store 4
+var store4 {
+  location: "Capitol Hill",
+  minCustHr: 20,
+  maxCustHr: 38,
+  avgCookieSale: 2.3
+};
+
+//store 5
+var store5 {
+  location: "Alki",
+  minCustHr: 2,
+  maxCustHr: 16,
+  avgCookieSale: 4.6
+};
